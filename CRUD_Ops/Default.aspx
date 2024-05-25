ce3d79f7d26acf4ebe1cd0212bfc04abcf500ab3
kishan1223prajapati <91361532+kishan1223prajapati@users.noreply.github.com>
2024-05-26 00:01:08 +0530

    Add project files.

---
+++ CRUD_Ops/Default.aspx
@@ -0,0 +1,60 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="CRUD_Ops._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <div>
+        <div style="font-size:x-large" align="center"> Student Info Manage Form</div>
+        <br />
+
+
+        <table class="w-100">
+            <tr>
+                <td style="width: 534px">Student ID</td>
+                <td>
+                    <asp:TextBox ID="Student_ID" runat="server" Font-Size="Medium" Width="363px"></asp:TextBox>
+                &nbsp;<asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="Student_ID" ErrorMessage="Mandatory Field"></asp:RequiredFieldValidator>
+                </td>
+            </tr>
+            <tr>
+                <td style="width: 534px; height: 41px;">Student Name</td>
+                <td style="height: 41px">
+                    <asp:TextBox ID="Student_Name" runat="server" Font-Size="Medium" Width="363px"></asp:TextBox>
+                &nbsp;</td>
+            </tr>
+            <tr>
+                <td style="width: 534px">Address</td>
+                <td>
+                    <asp:DropDownList ID="Student_Address" runat="server" Width="118px">
+                        <asp:ListItem Selected="True"></asp:ListItem>
+                        <asp:ListItem>U.K.</asp:ListItem>
+                        <asp:ListItem>India</asp:ListItem>
+                    </asp:DropDownList>
+                &nbsp;</td>
+            </tr>
+            <tr>
+                <td style="width: 534px">Age</td>
+                <td>
+                    <asp:TextBox ID="Student_Age" runat="server" Font-Size="Medium" Width="363px"></asp:TextBox>
+                &nbsp;</td>
+            </tr>
+            <tr>
+                <td style="width: 534px">Contact</td>
+                <td>
+                    <asp:TextBox ID="Student_Contact" runat="server" Font-Size="Medium" Width="363px"></asp:TextBox>
+                &nbsp;<asp:RegularExpressionValidator ID="RegularExpressionValidator1" runat="server" ControlToValidate="Student_Contact" ErrorMessage="It Should Be 10 Digits" ValidationExpression="^\d{10}$"></asp:RegularExpressionValidator>
+                </td>
+            </tr>
+            <tr>
+                <td style="width: 534px">&nbsp;</td>
+                <td>
+                    <asp:Button ID="Button1" runat="server" BackColor="#9933FF" Font-Bold="True" Font-Size="Medium" ForeColor="White" OnClick="Button1_Click" Text="INSERT" />
+                &nbsp;<asp:Button ID="Button2" runat="server" BackColor="#9933FF" Font-Bold="True" Font-Size="Medium" ForeColor="White" OnClick="Button2_Click" Text="UPDATE" />
+                &nbsp;<asp:Button ID="Button3" runat="server" BackColor="#9933FF" Font-Bold="True" Font-Size="Medium" ForeColor="White" OnClick="Button3_Click" Text="DELETE" />
+                &nbsp;<asp:Button ID="Button4" runat="server" BackColor="#9933FF" Font-Bold="True" Font-Size="Medium" ForeColor="White" OnClick="Button4_Click" Text="SEARCH" />
+                </td>
+            </tr>
+        </table>
+
+    </div>
+    <asp:GridView ID="GridView1" runat="server" OnSelectedIndexChanged="GridView1_SelectedIndexChanged" HorizontalAlign="center">
+    </asp:GridView>
+</asp:Content>
---
+++ CRUD_Ops/Default.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Services.Description;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CRUD_Ops
+{
+    public partial class _Default : Page
+    {
+        SqlConnection con = new SqlConnection("Data Source=LAPTOP-LTLSG7NR;Initial Catalog=CrudOpsDB;Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadRecord();
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand comm = new SqlCommand("INSERT INTO StudentInfo_Tab1 values('"+int.Parse(Student_ID.Text)+"','"+Student_Name.Text+"', '"+Student_Address.SelectedValue+"', '"+double.Parse(Student_Age.Text)+"','"+Student_Contact.Text+"')", con);
+            comm.ExecuteNonQuery();
+            con.Close();
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Successfully Inserted');", true);
+            LoadRecord();
+            ClearFields();
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        void LoadRecord()
+        {
+            SqlCommand comm = new SqlCommand("SELECT * FROM StudentInfo_Tab1", con);
+            SqlDataAdapter d = new SqlDataAdapter(comm);
+            DataTable dt = new DataTable();
+            d.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand comm = new SqlCommand("UPDATE StudentInfo_Tab1 SET StudentName = '"+Student_Name.Text+"', Address = '"+Student_Address.SelectedValue+"', Age = '"+double.Parse(Student_Age.Text)+"', Contact = '" +Student_Contact.Text + "' WHERE StudentID = '" + int.Parse(Student_ID.Text) + "'", con);
+            comm.ExecuteNonQuery();
+            con.Close();
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Successfully Updated');", true);
+            LoadRecord();
+            ClearFields();
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand comm = new SqlCommand("DELETE StudentInfo_Tab1 WHERE StudentID = '" + int.Parse(Student_ID.Text) + "'", con);
+            comm.ExecuteNonQuery();
+            con.Close();
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Successfully Deleted');", true);
+            LoadRecord();
+            ClearFields();
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            SqlCommand comm = new SqlCommand("SELECT * FROM StudentInfo_Tab1 WHERE StudentID = '" + int.Parse(Student_ID.Text) + "'", con);;
+            SqlDataAdapter d = new SqlDataAdapter(comm);
+            DataTable dt = new DataTable();
+            d.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+            ClearFields();
+        }
+
+        private void ClearFields()
+        {
+            Student_ID.Text = string.Empty;
+            Student_Name.Text = string.Empty;
+            Student_Address.ClearSelection(); 
+            Student_Age.Text = string.Empty;
+            Student_Contact.Text = string.Empty;
+        }
+    }
+}
